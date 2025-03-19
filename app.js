0411badd267f5dc396f16b6a017e93694d1bfe9f
mofesilva <mofesilva.dev@gmail.com>
2025-03-19 15:59:17 -0300

    Estilizando a homepage

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const instagramLaunch = document.querySelector('.fa-instagram');
 
 menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
