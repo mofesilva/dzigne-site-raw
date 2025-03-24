8a39bd4a75a803d0c64c7e48453d75b286918c71
mofesilva <mofesilva.dev@gmail.com>
2025-03-24 15:42:37 -0300

    Finalizando ajustes CSS da home page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector('#mobile-menu');
+const menu = document.querySelector('.mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const instagramLaunch = document.querySelector('.fa-instagram');
 
